69a94eef1ba2ff58d753b0b6760dfad16a9da416
Abigirl Muchineripi <muchineripiabby@gmail.com>
2024-07-21 06:02:25 +0200

    NodeJS Tutorial

---
+++ course24/index.js
@@ -0,0 +1,12 @@
+import  getPosts, {getPostsLength}  from "./postController.js";
+
+console.log(getPosts());
+
+console.log(`Posts Length : ${getPostsLength}`);
+/*console.log('Hello, World!');
+const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
+
+console.log(`Random Number : ${generateRandomNumber()}`);
+
+console.log(`Celsius to fahrenhiet: ${celciusToFahrenheit(0)}`);
+*/
